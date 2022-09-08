c9f65c3bd05932eeee91b5028d88ece1bbc05223
THANIYA JUNTHATIP <6150110019@psu.ac.th>
2022-09-08 21:13:30 +0700

    feat product

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { ProductModule } from './module/product/product.module';
+import { ProductController } from './module/product/product.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/module/product/product.module.ts
@@ -0,0 +1,10 @@
+import { ProductService } from './product.service';
+import { Module } from '@nestjs/common';
+import { ProductController } from './product.controller';
+
+@Module({
+  imports: [],
+  controllers: [ProductController],
+  providers: [ProductService],
+})
+export class ProductModule {}
